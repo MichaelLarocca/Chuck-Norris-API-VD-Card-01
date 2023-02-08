b024b63c809c753ce02f27ad1c1d4b7054eec153
Michael J. Larocca <michaellarocca@live.com>
2023-02-07 19:35:37 -0500

    Added throw new Error

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const displayJoke = document.getElementById("display-joke");
 
 async function fetchJoke() {
-	const results = await fetch(`https://api.chucknorris.io/jokes/random`);
+	const results = await fetch(`https://api.chucknorris.io/jokes/random/1`);
 	const data = await results.json();
-	// console.log(data.value);
+
+	if (!results.ok) {
+		displayJoke.textContent = `"DO NOT DISTURB!" Chuck Norris is currently entertaining guests in his hotel room.`;
+		throw new Error("Request failed.");
+	}
 	displayJoke.textContent = data.value;
 }
